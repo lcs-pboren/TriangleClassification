5cfbdd1ac44e8ffc1e304027c0588fa91ac6b9fa
Patrick Boren <Pboren@lcs.on.ca>
2021-12-11 10:02:00 -0500

    Initial Commit

---
+++ TriangleClassification/TriangleClassificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriangleClassificationApp.swift
+//  TriangleClassification
+//
+//  Created by Patrick Boren on 2021-12-11.
+//
+
+import SwiftUI
+
+@main
+struct TriangleClassificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
